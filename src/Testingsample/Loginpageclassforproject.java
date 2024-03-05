0c3e3b223be856233391f11393077fd7aa0355be
Amruta Jagtap <amruta.jagtap@globallogic.com>
2024-03-06 00:17:21 +0530

    commiting the code

---
+++ src/Testingsample/Loginpageclassforproject.java
@@ -0,0 +1,13 @@
+package Testingsample;
+
+public class Loginpageclassforproject {
+	public void testmethod()
+	
+	{
+		System.out.println("enter the username");
+		
+		System.out.println("enter the password");
+		
+	}
+
+}
